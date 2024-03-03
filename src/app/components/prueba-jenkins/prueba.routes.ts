6a4877acbdfea17a7a03a06783ea0e6bc488b6dc
romandono <maradono_tm@hotmail.com>
2024-03-03 21:17:40 +0100

    Añade links

--- src/app/components/prueba-jenkins/prueba.routes.ts
+++ src/app/components/prueba-jenkins/prueba.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { PruebaJenkinsComponent } from './prueba-jenkins.component';
 
 export const PRUEBA_ROUTES: Routes = [
-    {path: 'prueba-jenkins', component: PruebaJenkinsComponent}
+    {path: '', component: PruebaJenkinsComponent}
 ]
